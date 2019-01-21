a4460b32bd395eb982d662f76bea20e6659c4411
Michael Kroth <mkroth65@gmail.com>
2019-01-21 16:02:03 -0600

    Finished PUG template

--- Max Udemy/Templating/routes/shop.js
+++ Max Udemy/Templating/routes/shop.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
 	console.log('from the shop -> ', adminData.products);
 	const products = adminData.products;
-  res.render('shop', {prods: products, docTitle: 'Shop'})
+  res.render('shop', {prods: products, docTitle: 'Shop', path: '/shop'})
 });
 
 module.exports = router;
