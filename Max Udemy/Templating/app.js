65acbd301fd3203e8c33e0910926bea239b6c3bf
Michael Kroth <mkroth65@gmail.com>
2019-01-22 11:35:46 -0600

    Completed handlebars and ejs templates

--- Max Udemy/Templating/app.js
+++ Max Udemy/Templating/app.js
@@ -2,10 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+// const expressHbars = require('express-handlebars')
 const app = express();
 
-app.set('view engine', 'pug');
+// app.engine('handlebars', expressHbars());
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -18,7 +19,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render("404")//sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render("404", {docTitle: "Page Not Found"})//sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
